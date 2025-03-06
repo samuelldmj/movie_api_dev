e37ebcc71f0a082c8cdd1e91b0decb21e7111241
samuelldmj <samuelldmj5@gmail.com>
2025-03-06 16:03:39 +0100

    better error handling

--- src/controllers/addMovie.js
+++ src/controllers/addMovie.js
@@ -2,47 +2,27 @@
 const addMovieController = (req, res) => {
 
     //extracting payload from the body
-    const {movie_name, info, rating} = req.body;
+    const { movie_name, info, rating } = req.body;
 
     //validating the payload
-    // try {
+    try {
+        if (!movie_name) throw "Movie name is required"
+        if (!info) throw "Info is required"
+        if (rating < 1 || rating > 10) throw "Rating must be between 1-10"
+    } catch (err) {
 
-    // }catch(err){
-
-    // }
-
-    if(!movie_name){
         res.status(400).json({
-           status :  "failed",
-           message : "Movie name is required"
+            status: "failed",
+            message: err
         });
 
         return;
     }
 
-    if(!info){
-        res.status(400).json({
-           status :  "failed",
-           message : "Info is required"
-        });
-
-        return;
-    }
-
-    if(rating < 1 || rating > 10 ){
-        res.status(400).json({
-           status :  "failed",
-           message : "Rating must be between 1-10"
-        });
-
-        return;
-    }
- 
- 
     //return a response to the client.
     res.status(200).json({
-        status : "success",
-        message : "Movie Added Successfully"
+        status: "success",
+        message: "Movie Added Successfully"
     });
 }
 
